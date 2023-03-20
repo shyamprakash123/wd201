9d8ba6f1a3044381bb18e6b03ac2247070b76856
Shyam prakash <shyamprakash9959@gmail.com>
2023-03-20 12:31:47 +0530

    http-server modified

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,1 +1,15 @@
-console.log("hello github");
+const fs = require('fs')
+
+fs.writeFile(
+  'sample.txt',
+  'Hello World. Welcome to Node.js File System module.',
+  (err) => {
+    if (err) throw err
+    console.log('File created!')
+  }
+)
+
+fs.readFile('sample.txt', (err, data) => {
+  if (err) throw err
+  console.log(data.toString())
+})
---
+++ http-server/index.js
@@ -0,0 +1,51 @@
+const http = require("http");
+const fs = require("fs");
+const args = require("minimist")(process.argv.slice(2));
+const port =args["_"][0];
+
+let homeContent = "";
+let projectContent = "";
+let registration = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html", (err, reg) => {
+  if (err) {
+    throw err;
+  }
+  registration = reg;
+});
+
+
+http.createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/registration":
+        response.write(registration);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+        })
+        .listen(port);
+
