01eb27c3edd9ac04715db3b72725cf8b3c916109
Shyam prakash <shyamprakash9959@gmail.com>
2023-04-14 14:15:58 +0530

    updated to production

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -19,6 +19,10 @@ module.exports = (sequelize, DataTypes) => {
       });
     }
 
+    static getTodos() {
+      return this.findAll();
+    }
+
     markAsComplete() {
       return this.update({ completed: true });
     }
